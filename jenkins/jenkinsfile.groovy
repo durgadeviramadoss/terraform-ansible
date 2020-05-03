bf0db34ca602410a8e3b1f3184cba1bb12022f09
durgadeviramadoss <durgaoct1989@gmail.com>
2020-05-03 22:53:24 +0530

    Changed credentials path of gcp and gcs
    Changed credentials path of gcp and gcs

--- jenkins/jenkinsfile.groovy
+++ jenkins/jenkinsfile.groovy
@@ -6,11 +6,6 @@ pipeline {
             description: 'Terraform Executiomn type',
             name: 'REQUESTED_ACTION')
     }
-	environment {
-		GCP_CRED = credentials('gcp_credentials')
-		GCS_CRED = credentials('gcs_credentials')
-		PROJ_DIR = "${WORKSPACE}"
-	}
 	stages {
         stage("build") {
             steps {
@@ -21,7 +16,6 @@ pipeline {
 		stage("Execute Terraform Plan") {
 			steps{
 				sh '''
-					sed -i -e 's#/home/karthick/credentials/gcs-e10aec265420.json#'${GCS_CRED}'#' ansible-terraform-exec/verify-resources-tf.yml
 					ansible-playbook ansible-terraform-exec/verify-resources-tf.yml
 				'''
 			}
