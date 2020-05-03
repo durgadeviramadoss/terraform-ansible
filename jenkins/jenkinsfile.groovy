577a38153a18335ee7d0ebfd35ac63583609ed33
durgadeviramadoss <durgaoct1989@gmail.com>
2020-05-03 22:17:06 +0530

    Provided GCS credentials
    Provided GCS credentials

--- jenkins/jenkinsfile.groovy
+++ jenkins/jenkinsfile.groovy
@@ -21,6 +21,7 @@ pipeline {
 		stage("Execute Terraform Plan") {
 			steps{
 				sh '''
+					sed -i -e 's#/home/karthick/credentials/gcs-e10aec265420.json#'${GCS_CRED}'#' ansible-terraform-exec/verify-resources-tf.yml
 					ansible-playbook ansible-terraform-exec/verify-resources-tf.yml
 				'''
 			}
