a65f83c14ffc5182310d5b5e99e06f24835d3609
durgadeviramadoss <durgaoct1989@gmail.com>
2020-05-03 22:57:57 +0530

    Added stages for terraform states
    Added stages for terraform states

--- jenkins/jenkinsfile.groovy
+++ jenkins/jenkinsfile.groovy
@@ -14,10 +14,27 @@ pipeline {
             }
         }
 		stage("Execute Terraform Plan") {
+			when {
+                	expression { params.REQUESTED_ACTION == 'ansible-tf-plan' }
+            		}
 			steps{
-				sh '''
-					ansible-playbook ansible-terraform-exec/verify-resources-tf.yml
-				'''
+				sh 'ansible-playbook ansible-terraform-exec/verify-resources-tf.yml'
+			}
+		}
+		stage("Create Terraform Plan") {
+			when {
+                	expression { params.REQUESTED_ACTION == 'ansible-tf-apply' }
+            		}
+			steps{
+				sh 'ansible-playbook ansible-terraform-exec/create-resources-tf.yml'
+			}
+		}
+		stage("Destory Terraform Plan") {
+			when {
+                	expression { params.REQUESTED_ACTION == 'ansible-tf-destory' }
+            		}
+			steps{
+				sh 'ansible-playbook ansible-terraform-exec/destory-resources-tf.yml'
 			}
 		}
 	}
