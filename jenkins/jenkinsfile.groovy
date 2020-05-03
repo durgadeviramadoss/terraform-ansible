76ee95c23bf87fab1cbd6ff32aab9c8b3520baf1
durgadeviramadoss <durgaoct1989@gmail.com>
2020-05-03 11:23:07 +0000

    Adding jenkinsfile

---
+++ jenkins/jenkinsfile.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+    parameters {
+		choice(
+            choices: 'ansible-tf-plan\nansible-tf-apply\nansible-tf-destory',
+            description: 'Terraform Executiomn type'
+            name: 'REQUESTED_ACTION')
+    }
+	environment {
+		GCP_CRED = credentials('gcp_credentials')
+		GCS_CRED = credentials('gcs_credentials')
+	}
+	stages {
+        stage("build") {
+            steps {
+                cleanWs()
+                checkout scm
+            }
+        }
+		stage("shell") {
+			steps{
+				echo $PWD 
+			}
+		}
+	}
+}
